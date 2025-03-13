02daf4433594152db5354898b85c09796667cd09
DFTFries <dftfries@gmail.com>
2025-03-13 19:22:17 +0100

    New

---
+++ 12-string-methods.py
@@ -0,0 +1,19 @@
+#
+
+# name = input("Enter your full name: ")
+# phone_number = input("Enter your phone number: ")
+
+# result = len(name)
+# result = name.find("m")
+# result = name.rfind("n")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()  <- True or False depending wether the the string contains only alphabetical symbols
+# result = phone_number.count("/")
+# phone_number = phone_number.replace("/", "")
+
+# print(phone_number)
+
+print(help(str))
