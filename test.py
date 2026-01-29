dbda3dbc57d2fb2b4b9699a11f5ece24b6177c5d
DFTFries <dftfries@gmail.com>
2026-01-29 12:11:52 +0100

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-time = int(input("Enter the amount of hours worked total: "))
-tip_received = float(input("Enter the amount of tip: "))
-
-tip = tip_received / time
-
-print(f"Everybody receives a tip of {tip:.2f}€ per hour.")
- 
