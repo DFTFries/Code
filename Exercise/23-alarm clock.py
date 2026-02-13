1773da85fecd54ac496d63ae93533ecb9d07d2a2
DFTFries <dftfries@gmail.com>
2026-02-13 23:30:16 +0100

    update

--- Exercise/23-alarm clock.py
+++
@@ -1,8 +0,0 @@
-# Python Alarm Clock
-
-import time
-import datetime
-import pygame
-
-def set_alarm(alarm_time):
-    pass
---
+++ Exercise/23-alarm clock/23-alarm clock.py
@@ -0,0 +1,33 @@
+# Python Alarm Clock
+
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "Exercise/23-alarm clock/Dua_Lipa-Physical__Remixes_-LODATO__Remix__Extended-72436644.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("WAKE UP! 😫")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.set_volume(0.1)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+
+            is_running = False
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
