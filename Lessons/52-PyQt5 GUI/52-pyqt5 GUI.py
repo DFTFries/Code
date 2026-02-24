10657f837abe37057010a74428c22a16655794c1
DFTFries <dftfries@gmail.com>
2026-02-24 18:31:26 +0100

    update

--- Lessons/52-PyQt5 GUI/52-pyqt5 GUI.py
+++ Lessons/52-PyQt5 GUI/52-pyqt5 GUI.py
@@ -2,16 +2,15 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QIcon, QPixmap
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Test GUI")
-        self.setGeometry(700, 300, 500, 500)
-        self.setWindowIcon(QIcon("Lessons/52-PyQt5 GUI intro/AWW - 112px.png"))
+        self.setGeometry(500, 100, 500, 100)
+        self.setWindowIcon(QIcon("Lessons/52-PyQt5 GUI/AWW.png"))
 
         label = QLabel("Hello", self)
         label.setFont(QFont("Arial", 40))
@@ -21,6 +20,25 @@ class MainWindow(QMainWindow):
                             "font-weight: bold;"
                             "font-style: italic;"
                             "text-decoration: underline;")
+        
+        image = QLabel(self)
+        image.setGeometry(0, 0, 100, 100)
+
+        pixmap = QPixmap("Lessons/52-PyQt5 GUI/AWW.png")
+        image.setPixmap(pixmap)
+
+        image.setScaledContents(True)
+
+        image.setGeometry(self.width() - image.width(),
+                           0, 
+                           image.width(), 
+                           image.height())
+
+        # Center Image
+        # image.setGeometry((self.width() - image.width()) // 2,
+        #                    (self.height() - image.height()) // 2, 
+        #                    image.width(), 
+        #                    image.height())
         
         # label.setAlignment(Qt.AlignTop)       # vertically top
         # label.setAlignment(Qt.AlignBottom)    # vertically bottom
@@ -33,6 +51,7 @@ class MainWindow(QMainWindow):
         # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) # center & center
         label.setAlignment(Qt.AlignCenter)  # center & center
 
+        
 
 def main():
     app = QApplication(sys.argv) 
